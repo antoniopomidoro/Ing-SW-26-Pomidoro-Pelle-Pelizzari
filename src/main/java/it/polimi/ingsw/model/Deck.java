6d867d5bc9d7efc7c2fba96151937034d037f511
alessandro <alessandropelle1608@gmail.com>
2026-03-10 13:32:29 +0100

    first implementation of deck functionality, in pom.xml i updated the Maven integrated compiler to java 21(in the section plugins) for a method of arraylist implemented in java21(removeFirst)

--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -1,29 +1,87 @@
 package it.polimi.ingsw.model;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+
+
+
 
 public class Deck {
-    private List<Card> age_1_Cards;
-    private List<Card> age_2_Cards;
-    private List<Card> age_3_Cards;
-    private List<Building> age_1_Buildings;
-    private List<Building> age_2_Buildings;
-    private List<Building> age_3_Buildings;
-
+    private ArrayList<Card> deckE1;
+    private ArrayList<Card> deckE2;
+    private ArrayList<Card> deckE3;
+    private ArrayList<Building> deckB1;
+    private ArrayList<Building> deckB2;
+    private ArrayList<Building> deckB3;
     public Deck() {
         // Skeleton constructor
     }
 
-    public boolean shuffle() {
-        return false;
+    public void shuffle() {
+        Collections.shuffle(deckE1);
+        Collections.shuffle(deckE2);
+        Collections.shuffle(deckE3);
+        return;
     }
 
-    public Card popCard(Age age) {
-        return null;
+    //add a card to the characters deck in the specified Age
+    public boolean addCard(Card c, Age a) throws BuildingInDeckException {
+        if(c.getClass() == Building.class){
+            throw new BuildingInDeckException();
+        }
+        switch(a){
+            case AGE_1:
+                deckE1.add(c);
+                return true;
+            case AGE_2:
+                deckE2.add(c);
+                return true;
+            case AGE_3:
+                deckE3.add(c);
+                return true;
+            default:
+                return false;
+        }
+
+    }
+    public boolean addBuilding(Building b, Age a){
+        switch (a){
+            case AGE_1:
+                deckB1.add(b);
+                return true;
+            case AGE_2:
+                deckB2.add(b);
+                return true;
+            case AGE_3:
+                deckB3.add(b);
+                return true;
+            default:
+                return false;
+        }
+
+
+    }
+//return the first card on the deck if the deck is empty throws an exception
+    public Card popCard(Age a) {
+        switch(a){
+            case AGE_1:
+                return deckE1.removeFirst();
+            case AGE_2:
+                return deckE2.removeFirst();
+            case AGE_3:
+                return deckE3.removeFirst();
+            default:
+                return null;
+        }
+
     }
 
-    public Building popBuilding(Age age) {
-        return null;
+    public Building getBuildings(Age age) {
+             return null;
     }
+
+    public static class BuildingInDeckException extends Exception {
+
+    }
+
 }
