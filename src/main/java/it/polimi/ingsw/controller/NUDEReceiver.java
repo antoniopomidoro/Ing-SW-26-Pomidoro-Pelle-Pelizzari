c35bedd26f94312168c989c5e286c7ed92c66eb8
alepelle <alessandropelle1608@gmail.com>
2026-03-24 17:05:24 +0100

    added executor interfaces

--- src/main/java/it/polimi/ingsw/controller/NUDEReceiver.java
+++ src/main/java/it/polimi/ingsw/controller/NUDEReceiver.java
@@ -1,5 +1,26 @@
 package it.polimi.ingsw.controller;
 import com.fasterxml.jackson.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.polimi.ingsw.model.game.GameState;
+
+
 public class NUDEReceiver {
+   private ObjectMapper mapper;
+
+
+   public NUDEReceiver() {
+       this.mapper = new ObjectMapper();
+
+   }
+
+   public boolean execute(String rawJson){
+
+       return true;
+   }
+
+
+
+
+
 
 }
